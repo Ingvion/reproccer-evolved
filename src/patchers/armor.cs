8613b9a08b70b1aee83e7482b4cfdb1867629c1a
Ingvion <76794313+Ingvion@users.noreply.github.com>
2025-11-24 02:23:07 +0200

    Update GMST implementation

--- src/patchers/armor.cs
+++ src/patchers/armor.cs
@@ -18,7 +18,28 @@ public static class Armor
 
     public static void Run()
     {
-		
+        UpdateGMST();
 	}
+	
+    private static void UpdateGMST()
+    {
+        FormKey armorScalingFactor = Executor.Statics!
+        .First(elem => elem.Id == "ArmorScalingFactor")
+        .Formkey;
+
+        IGameSettingGetter conflictWinner = State.LinkCache.Resolve<IGameSettingGetter>(armorScalingFactor);
+        GameSetting record = State.PatchMod.GameSettings.GetOrAddAsOverride(conflictWinner);
+
+        if (record is GameSettingFloat gmstArmorScalingFactor) gmstArmorScalingFactor.Data = Settings.Armor.ArmorScalingFactor;
+
+        FormKey maxArmorRating = Executor.Statics!
+        .First(elem => elem.Id == "MaxArmorRating")
+        .Formkey;
+
+        conflictWinner = State.LinkCache.Resolve<IGameSettingGetter>(maxArmorRating);
+        record = State.PatchMod.GameSettings.GetOrAddAsOverride(conflictWinner);
+
+        if (record is GameSettingFloat gmstMaxArmorRating) gmstMaxArmorRating.Data = Settings.Armor.MaxArmorRating;
+    }
 }
 
