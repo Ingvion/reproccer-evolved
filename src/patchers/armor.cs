0e66a2ce6519afd9d6278e43c9e6ba237915ca10
Ingvion <76794313+Ingvion@users.noreply.github.com>
2025-11-24 02:20:11 +0200

    BUGFIX: patcher ignores settings.json
    Incorrect settings storage.

--- src/patchers/armor.cs
+++ src/patchers/armor.cs
@@ -2,6 +2,7 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
+using ReProccer.Config;
 using ReProccer.Utils;
 using System.Text.Json.Nodes;
 
@@ -10,7 +11,7 @@ namespace ReProccer.Patchers;
 public static class Armor
 {
     private static readonly IPatcherState<ISkyrimMod, ISkyrimModGetter> State = Executor.State!;
-    private static readonly Lazy<Config.Armor> Settings = new();
+    private static readonly AllSettings Settings = Executor.Settings!;
     private static readonly JsonObject Rules = Executor.Rules!["armor"]!.AsObject();
 
     private static Utils.PatchingData RecordData;
