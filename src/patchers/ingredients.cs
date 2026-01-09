7a2979c7ce92194a6a74156d7e7e6a8182026126
Ingvion <76794313+Ingvion@users.noreply.github.com>
2026-01-09 16:29:27 +0200

    Ingredients patcher stub

---
+++ src/patchers/ingredients.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Nodes;
+
+namespace ReProccer.Patchers;
+
+public static class IngredientsPatcher
+{
+    private static readonly JsonObject Rules = Executor.Rules!["alchemy"]!.AsObject();
+
+    public static void Run()
+    {
+        List<List<string>> blacklists = [
+            [.. Rules["excludedEffects"]!.AsArray().Select(value => value!.GetValue<string>())],
+            [.. Rules["excludedIngredients"]!.AsArray().Select(value => value!.GetValue<string>())]
+        ];
+    }
+}
