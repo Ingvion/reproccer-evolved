fa4495d8ec27d56e00e38b2c0f19e46ca90aad62
Ingvion <76794313+Ingvion@users.noreply.github.com>
2026-01-12 14:10:53 +0200

    Effects patcher

--- src/patchers/ingredients.cs
+++ src/patchers/ingredients.cs
@@ -16,6 +16,14 @@ public static class IngredientsPatcher
     {
         List<IIngredientGetter> records = GetRecords();
         List<string> blacklist = [.. Rules["excludedEffects"]!.AsArray().Select(value => value!.GetValue<string>())];
+
+        foreach (var record in records)
+        {
+            Logger = new Logger();
+            PatchEffects(record, blacklist);
+
+            ShowReport(record);
+        }
     }
 
     /// <summary>
@@ -31,6 +39,7 @@ public static class IngredientsPatcher
 
         List<IIngredientGetter> validRecords = [];
         List<string> excludedNames = [.. Rules["excludedIngredients"]!.AsArray().Select(value => value!.GetValue<string>())];
+        List<string> excludedEffects = [.. Rules["excludedEffects"]!.AsArray().Select(value => value!.GetValue<string>())];
 
         foreach (var record in conflictWinners)
         {
@@ -74,6 +83,108 @@ public static class IngredientsPatcher
                 Logger.Info($"Found in the \"No patching\" list by name");
                 ShowReport(record);
             }
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void PatchEffects(IIngredientGetter ingr, List<string> excludedValues)
+    {
+        // just in case (it's possible to remove all effects and/or effects' container with xEdit/zEdit)
+        if (ingr.Effects is null || ingr.Effects.Count == 0)
+        {
+            Logger.Error($"The ingredient must have at least 1 effect, found none");
+            return;
+        }
+
+        Ingredient? newIngr = null;
+        for (int i = 0; i < ingr.Effects.Count; i++)
+        {
+            bool newDuration = false;
+            bool newMagnitude = false;
+
+            Executor.State!.LinkCache.TryResolve(ingr.Effects[i].BaseEffect, out var mgef);
+            if (mgef is not null && mgef.IsPatchable(excludedValues))
+            {
+                JsonNode? ruleNode;
+                float? ruleData;
+
+                if (!mgef.Flags.HasFlag(MagicEffect.Flag.NoDuration))
+                {
+                    ruleNode = Helpers.RuleByName(mgef.Name!.ToString()!, Rules["effects"]!.AsArray(), data1: "names", data2: "duration");
+                    ruleData = ruleNode?.AsType<float>();
+
+                    if (ruleData is not null && ingr.Effects[i].Data!.Duration != ruleData)
+                    {
+                        newIngr = Executor.State!.PatchMod.Ingredients.GetOrAddAsOverride(ingr);
+                        newIngr.Effects[i].Data!.Duration = (int)ruleData;
+                        newDuration = true;
+                    }
+                }
+
+                if (!mgef.Flags.HasFlag(MagicEffect.Flag.NoMagnitude))
+                {
+                    ruleNode = Helpers.RuleByName(mgef.Name!.ToString()!, Rules["effects"]!.AsArray(), data1: "names", data2: "magnitudeMult");
+                    ruleData = ruleNode?.AsType<float>();
+
+                    if (ruleData is not null && ruleData > 0 && ingr.Effects[i].Data!.Magnitude != (ingr.Effects[i].Data!.Magnitude * ruleData))
+                    {
+                        newIngr = Executor.State!.PatchMod.Ingredients.GetOrAddAsOverride(ingr);
+                        newIngr.Effects[i].Data!.Magnitude *= (float)ruleData;
+                        newMagnitude = true;
+                    }
+                }
+            }
+
+            if (newIngr is not null)
+            {
+                Logger.Info($"{mgef!.Name}: " +
+                    $"duration {ingr.Effects[i].Data!.Duration}{(newDuration ? " -> " + newIngr!.Effects[i].Data!.Duration : "")}, " +
+                    $"magnitude {ingr.Effects[i].Data!.Magnitude}{(newMagnitude ? " -> " + (decimal)newIngr!.Effects[i].Data!.Magnitude : "")}", true); 
+            }
+        }
+
+        if (Settings.Ingredients.PriceLimits)
+        {
+            uint newValue = (uint)Math.Clamp(ingr.Value, Settings.Ingredients.MinValue, Settings.Ingredients.MaxValue);
+            if (newValue != ingr.Value)
+            {
+                newIngr = Executor.State!.PatchMod.Ingredients.GetOrAddAsOverride(ingr);
+                newIngr.Value = newValue;
+                Logger.Info($"Value: {ingr.Value} -> {newIngr.Value}", true, true);
+            }
+        }
+    }
+
+    private static bool IsPatchable(this IMagicEffectGetter mgef, List<string> excludedValues)
+    {
+        // effect has no name
+        if (mgef.Name is null) return false;
+
+        // archetypes are restricted, and mgef's archetype is not value mod or peak value mod
+        if (Settings.Ingredients.RestrictArchetypes && 
+            mgef.Archetype.Type != MagicEffectArchetype.TypeEnum.ValueModifier &&
+            mgef.Archetype.Type != MagicEffectArchetype.TypeEnum.PeakValueModifier)
+        {
+            return false;
+        }
+
+        // found in the excluded effects list by edID
+        if (Settings.General.ExclByEdID && mgef.EditorID!.IsExcluded(excludedValues, true))
+        {
+            if (Settings.Debug.ShowExcluded)
+                Logger.Info($"Found in the excluded effects list by EditorID");
+
+            return false;
+        }
+
+        // found in the excluded effects list by name
+        if (mgef.Name.ToString()!.IsExcluded(excludedValues))
+        {
+            if (Settings.Debug.ShowExcluded)
+                Logger.Info($"Found in the excluded effects list by name");
+
             return false;
         }
 
